8ce2bf77d5edd8630845aadadeb0cc456bd6479e
Dillon Teakell <teakelld@icloud.com>
2024-04-09 11:28:16 -0400

    Initial Commit

---
+++ Unit Conversion/Unit_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConversionApp.swift
+//  Unit Conversion
+//
+//  Created by Dillon Teakell on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
